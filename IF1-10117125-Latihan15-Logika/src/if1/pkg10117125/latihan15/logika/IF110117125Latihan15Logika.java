908ee1ccc196109de2a167f4c548ea616aede34e
zulfiihzam <35595962+zulfiihzam@users.noreply.github.com>
2020-10-21 00:58:04 +0700

    Add files via upload

---
+++ IF1-10117125-Latihan15-Logika/src/if1/pkg10117125/latihan15/logika/IF110117125Latihan15Logika.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10117125.latihan15.logika;
+
+/**
+ *
+ * @author
+ * Nama  : Zulfi Ihzam Rahmat
+ * Kelas : IF 1
+ * NIM   : 10117125
+ * Deskripsi Program : 
+ */
+public class IF110117125Latihan15Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        boolean a = true;
+        boolean b = false;
+        System.out.println("a && b = " + (a && b));
+        System.out.println("a || b = " + (a || b));
+        System.out.println("!a = " + !a);
+        System.out.println("!(a && b) = " + !(a && b));
+    }
+    
+}
